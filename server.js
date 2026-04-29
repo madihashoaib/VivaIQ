af6150049e1df39f99984e266b296ac5e64f5366
madihashoaib <madihashuaib2023@gmail.com>
2026-04-29 20:41:10 +0500

    fix static path for vercel

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.get('/api/test', (req, res) => {
